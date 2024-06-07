09f10b4ce7d82434b800b872e5fea5bea1fbc509
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2024-06-07 15:38:42 +0100

    removed openai key

--- src/app/utils/openai.ts
+++
@@ -1,9 +0,0 @@
-export const IMAGE_MODEL = "dall-e-2";
-
-const OpenAI = require("openai");
-
-const openai = new OpenAI({
-  key: "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",
-});
-
-export default openai;
