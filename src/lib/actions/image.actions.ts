536d3f3efe24664706822dfa3524beb3ceae8d1d
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2024-06-13 14:46:53 +0100

    deploy v4

--- src/lib/actions/image.actions.ts
+++ src/lib/actions/image.actions.ts
@@ -1,10 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
-import User from "../database/models/user.model";
-import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
 
 import { v2 as cloudinary } from "cloudinary";
